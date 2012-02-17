1dd58bc75978877970af863a2ed3222464f6add2
U-root-PC\root <root@root-PC.(none)>
2012-02-17 03:16:21 -0500

    Shapes working OK. PWM still wonky.

--- main/shapes.c
+++ main/shapes.c
@@ -3,13 +3,15 @@
 #include "extern.h"
 
 #define SQUARE_SIDE_DELAY 500000
-#define QUARTER_TURN_DELAY 130000
+#define QUARTER_TURN_DELAY 150000
 #define SQUARE_IDLE_DELAY 400000
-#define THREE_CIRCLES_DELAY 5000000
-
+#define THREE_CIRCLES_DELAY 5600000
+#define FE_LINE_DELAY 350000
+#define FE_TURN_DELAY 1300000
+#define FE_LEFT 200000
 void circle(void){
 	long delay = THREE_CIRCLES_DELAY;
-	lcd_puts(LCD_LINE2, "CIRCLEZ!");
+	lcd_puts(LCD_LINE2, "CIRCLES!");
 	right_pwm_high = PWM_FRAME_LEN/2;
 	left_pwm_high = PWM_FRAME_LEN/4;
 	right_pwm_state = 1;
@@ -24,7 +26,7 @@ void circle(void){
 void square(void){
 	long delay;
 	int i = 0;
-	lcd_puts(LCD_LINE2, "SQUAHURR");
+	lcd_puts(LCD_LINE2, "SQUARES!");
 	right_pwm_state = 1;
 	left_pwm_state = 1;
 	for(i = 0; i < 7; i++){
@@ -59,6 +61,66 @@ void square(void){
 		while(S3 && --delay){
 			_asm("nop");
 	}
+	min_throttle();
+	lcd_puts(LCD_LINE2, "CHILLING");
+}
+
+void fig_eight(void){
+	long delay = FE_LINE_DELAY + 50000;
+	lcd_puts(LCD_LINE2, "FIGURE 8");
+	right_pwm_state = 1;
+	left_pwm_state = 1;
+	both_throttle(.5);
+	while(S1 && --delay){
+		_asm("nop");
+	}
+	
+	delay = FE_TURN_DELAY;
+	right_pwm_high = PWM_FRAME_LEN/5;
+	left_pwm_high = PWM_FRAME_LEN/2;
+	while(S1 && --delay){
+		_asm("nop");
+	}
+	
+	delay = FE_LINE_DELAY;
+	both_throttle(.5);
+	while(S1 && --delay){
+		_asm("nop");
+	}
+	
+	delay = FE_TURN_DELAY-FE_LEFT;
+	right_pwm_high = PWM_FRAME_LEN/2;
+	left_pwm_high = PWM_FRAME_LEN/5;
+	while(S1 && --delay){
+		_asm("nop");
+	}
+	
+	delay = FE_LINE_DELAY;
+	both_throttle(.5);
+	while(S1 && --delay){
+		_asm("nop");
+	}
+	
+	delay = FE_TURN_DELAY;
+	right_pwm_high = PWM_FRAME_LEN/5;
+	left_pwm_high = PWM_FRAME_LEN/2;
+	while(S1 && --delay){
+		_asm("nop");
+	}
+	
+	delay = FE_LINE_DELAY;
+	both_throttle(.5);
+	while(S1 && --delay){
+		_asm("nop");
+	}
+	
+	delay = FE_TURN_DELAY-FE_LEFT;
+	right_pwm_high = PWM_FRAME_LEN/2;
+	left_pwm_high = PWM_FRAME_LEN/5;
+	while(S1 && --delay){
+		_asm("nop");
+	}
+	
 	min_throttle();
 	lcd_puts(LCD_LINE2, "CHILLING");
 }
